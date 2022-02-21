b7f2ec2bb76d5fb348791eb184d87e5969322a55
pengpai <tejk4u123>
2022-02-21 09:56:29 +0800

    common

---
+++ icreditstudio-dataapi/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/common/SysUser.java
@@ -0,0 +1,16 @@
+package com.jinninghui.datasphere.icreditstudio.dataapi.common;
+
+/**
+ * @author Peng
+ */
+public class SysUser {
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
