439b8dfbc14a7472d74c6b23d669f8584801852c
liyanhui <liyanhui@hashvictor.com>
2022-02-21 12:03:55 +0800

    修改开发环境子应用domain

--- icreditstudio-dataapi-frontend/src/main.js
+++ icreditstudio-dataapi-frontend/src/main.js
@@ -56,7 +56,7 @@ function render(props = {}) {
   const { container } = props
   watcher(props)
   router = new VueRouter({
-    base: window.__POWERED_BY_QIANKUN__ ? '/subapp/datasource/' : '/',
+    base: window.__POWERED_BY_QIANKUN__ ? '/subapp/dataapi/' : '/',
     mode: 'history',
     routes
   })
