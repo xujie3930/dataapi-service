534d270339240cdc3da55efa7bfb61a8597aef88
xujie <xujie-bl@bestpay.com.cn>
2022-02-22 10:59:37 +0800

    --story=1022395 --user=徐捷 【数据服务】-API开发与管理-左侧树 https://www.tapd.cn/64558269/s/1422263

--- icreditstudio-dataapi/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/web/request/ApiGroupSaveRequest.java
+++ icreditstudio-dataapi/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/web/request/ApiGroupSaveRequest.java
@@ -1,6 +1,7 @@
 package com.jinninghui.datasphere.icreditstudio.dataapi.web.request;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * @author xujie
@@ -12,6 +13,7 @@ public class ApiGroupSaveRequest {
     //业务流程id
     private String workId;
     //api分组名称
+    @Length(min = 2, max = 50, message = "API分组名称为2~50个字")
     private String name;
     //api分组描述
     private String desc;
