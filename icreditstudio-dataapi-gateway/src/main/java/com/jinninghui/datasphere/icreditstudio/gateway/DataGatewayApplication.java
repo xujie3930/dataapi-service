6e83e00372eebad295686d137b947595236651cb
xujie <xujie-bl@bestpay.com.cn>
2022-03-01 11:26:12 +0800

    新增gateway模块，去除数据源、nacos等依赖

---
+++ icreditstudio-dataapi-gateway/src/main/java/com/jinninghui/datasphere/icreditstudio/gateway/DataGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.jinninghui.datasphere.icreditstudio.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Peng
+ */
+@SpringBootApplication
+@ComponentScan("com.jinninghui")
+public class DataGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataGatewayApplication.class, args);
+    }
+}
