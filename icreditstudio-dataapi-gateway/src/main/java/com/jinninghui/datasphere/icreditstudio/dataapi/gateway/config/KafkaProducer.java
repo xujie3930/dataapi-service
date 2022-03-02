9ca45699e739bcc12b4c710e009fc82b26ed9d43
xujie <xujie-bl@bestpay.com.cn>
2022-03-02 17:19:42 +0800

    增加kafka依赖

---
+++ icreditstudio-dataapi-gateway/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/gateway/config/KafkaProducer.java
@@ -0,0 +1,48 @@
+package com.jinninghui.datasphere.icreditstudio.dataapi.gateway.config;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+/**
+ * @author xujie
+ * @description kafka生产这
+ * @create 2022-03-02 16:51
+ **/
+@Component
+@Slf4j
+public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    //自定义topic
+    public static final String TOPIC = "topic.test";
+
+    public void send(Object obj) {
+        String obj2String = JSONObject.toJSONString(obj);
+        log.info("准备发送消息为：{}", obj2String);
+        //发送消息
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(TOPIC, obj);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                //发送失败的处理
+                log.info(TOPIC + " - 生产者 发送消息失败：" + throwable.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> stringObjectSendResult) {
+                //成功的处理
+                log.info(TOPIC + " - 生产者 发送消息成功：" + stringObjectSendResult.toString());
+            }
+        });
+
+
+    }
+}
