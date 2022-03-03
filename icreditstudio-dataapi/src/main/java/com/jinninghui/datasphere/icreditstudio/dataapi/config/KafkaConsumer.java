a42021a66b85d9d21f5df095b0729c2524751e85
xujie <xujie-bl@bestpay.com.cn>
2022-03-03 09:53:09 +0800

    提交代码

--- icreditstudio-dataapi/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/config/KafkaConsumer.java
+++ icreditstudio-dataapi/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/config/KafkaConsumer.java
@@ -28,15 +28,4 @@ public class KafkaConsumer {
             ack.acknowledge();
         }
     }
-
-    @KafkaListener(groupId = "test1",topics = KafkaProducer.TOPIC)
-    public void topic_test1(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-
-        Optional message = Optional.ofNullable(record.value());
-        if (message.isPresent()) {
-            Object msg = message.get();
-            log.info("topic_test1 消费了： Topic:" + topic + ",Message:" + msg);
-            ack.acknowledge();
-        }
-    }
 }
