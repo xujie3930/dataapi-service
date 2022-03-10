28f8def7c11c62c1e31c8662c8aed4f20c996ba8
lizheng <lizheng@newspiral.net>
2022-03-10 11:40:06 +0800

    【fix】: --bug=1022425 --user=李铮 【API详情】API详情中的参数字段名称应与后端返回值保持一致 https://www.tapd.cn/64558269/s/1441792

--- icreditstudio-dataapi-frontend/src/configuration/table/data-service-api-detail.js
+++ icreditstudio-dataapi-frontend/src/configuration/table/data-service-api-detail.js
@@ -28,8 +28,8 @@ export default {
     {
       type: 'text',
       label: '字段名称',
-      prop: 'fieldName',
-      formatter: ({ fieldName }) => fieldName?.toLocaleLowerCase() ?? ''
+      prop: 'fieldName'
+      // formatter: ({ fieldName }) => fieldName?.toLocaleLowerCase() ?? ''
     },
 
     {
