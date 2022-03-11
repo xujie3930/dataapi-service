e4ae1ee118c2139728212eb9c97849187f24e54e
xujie <xujie-bl@bestpay.com.cn>
2022-03-11 13:56:31 +0800

    提交配置文件

--- icreditstudio-dataapi-gateway/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/gateway/common/KafkaProducer.java
+++ icreditstudio-dataapi-gateway/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/gateway/common/KafkaProducer.java
@@ -1,8 +1,9 @@
-package com.jinninghui.datasphere.icreditstudio.dataapi.kafka;
+package com.jinninghui.datasphere.icreditstudio.dataapi.gateway.common;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
@@ -20,28 +21,29 @@ public class KafkaProducer {
 
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
+    public static String topic;
 
-    //自定义topic
-    public static final String TOPIC = "apiInvoke";
-//    public static final Integer PARTITION = 1;
-//    public static final String Key = "api";
+    @Value("${kafkaProducer.topic}")
+    public static void setTopic(String topicConfig) {
+        topic = topicConfig;
+    }
 
     public void send(Object obj) {
         String obj2String = JSON.toJSONString(obj);
         log.info("准备发送消息为：{}", obj2String);
         //发送消息
-        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(TOPIC, obj2String);
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(topic, obj2String);
         future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
             @Override
             public void onFailure(Throwable throwable) {
                 //发送失败的处理
-                log.info(TOPIC + " - 生产者 发送消息失败：" + throwable.getMessage());
+                log.info(topic + " - 生产者 发送消息失败：" + throwable.getMessage());
             }
 
             @Override
             public void onSuccess(SendResult<String, Object> stringObjectSendResult) {
                 //成功的处理
-                log.info(TOPIC + " - 生产者 发送消息成功：" + stringObjectSendResult.toString());
+                log.info(topic + " - 生产者 发送消息成功：" + stringObjectSendResult.toString());
             }
         });
 
