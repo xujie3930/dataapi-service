84ca9378da1bbd4b3229b8bfb62b02e384faf2ba
maoshi <maoshi@newspiral.net>
2022-06-15 17:24:56 +0800

    测试连接超时

--- icreditstudio-dataapi/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/service/impl/IcreditAppGroupServiceImpl.java
+++ icreditstudio-dataapi/src/main/java/com/jinninghui/datasphere/icreditstudio/dataapi/service/impl/IcreditAppGroupServiceImpl.java
@@ -68,7 +68,7 @@ public class IcreditAppGroupServiceImpl extends ServiceImpl<IcreditAppGroupMappe
     @ResultReturning
     public BusinessResult<List<AppGroupQueryListResult>> getList(AppQueryListRequest request) {
         try {
-            Thread.sleep(9000);
+            Thread.sleep(30000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
